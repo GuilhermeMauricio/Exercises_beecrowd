f52eb9ae2bbb3ff14e806e6ade020f36a9f85a46
Guilherme <guilhermemauriciolima@gmail.com>
2024-05-03 19:40:46 -0300

    Avarage att

--- avarege.py
+++ avarege.py
@@ -8,12 +8,8 @@ n4=float(n4)
 avg=((n1*2)+(n2*3)+(n3*4)+(n4*1))/(1+2+3+4)
 print(f'Media: {avg:.1f}')
 
-if avg>7:
-    print("Aluno aprovado.")
-if avg<5:
-    print("Aluno reprovado.")
-    
-if avg>5 or avg==5 and avg <7 or avg==7:
+
+if avg>=5 and avg<=7:
     print("Aluno em exame.")
     ne=float(input(''))
     print("Nota do exame: ",ne)
@@ -23,3 +19,9 @@ if avg>5 or avg==5 and avg <7 or avg==7:
     if avgpos<4.9:
         print("Aluno reprovado.")
     print('Media final: ',avgpos)
+
+if avg>7:
+    print("Aluno aprovado.")
+if avg<5:
+    print("Aluno reprovado.")
+    
