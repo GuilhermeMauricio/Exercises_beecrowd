16e4a73ef09b648dad1656dc07279489c82d3f6e
Guilherme <guilhermemauriciolima@gmail.com>
2024-05-14 22:44:28 -0300

    update

--- Sort.py
+++ Sort.py
@@ -2,9 +2,73 @@ n1, n2 , n3 = input(" "). split()
 n1=int(n1)
 n2=int(n2)
 n3=int(n3)
-lista=[]
-numeros=lista.append(n1)
-numeros=lista.append(n2)
-numeros=lista.append(n3)
-print(lista.sort())
-print(lista)
+
+if n1<n2 and n1<n3:
+    print(n1)
+    if n2<n3:
+        print(n2)
+        print(n3)
+    if n3<n2:
+        print(n3)
+        print(n2)
+       
+if n2<n1 and n2<n3:
+    print(n2)
+    if n1<n3:
+        print(n1)
+        print(n3)
+    if n3<n1:
+        print(n3)
+        print(n1)
+
+if n3<n1 and n3<n1:
+    print(n3)
+    if n1<n2:
+        print(n1)
+        print(n2)
+    if n2<n1:
+        print(n2)
+        print(n1)
+
+
+print(" ")
+print(n1)
+print(n2)
+print(n3)
+
+n1, n2, n3 = input("").split()
+n1 = int(n1)
+n2 = int(n2)
+n3 = int(n3)
+
+if n1 < n2 and n1 < n3:
+    print(n1)
+    if n2 < n3:
+        print(n2)
+        print(n3)
+    if n3 < n2:
+        print(n3)
+        print(n2)
+
+if n2 < n1 and n2 < n3:
+    print(n2)
+    if n1 < n3:
+        print(n1)
+        print(n3)
+    if n3 < n1:
+        print(n3)
+        print(n1)
+
+if n3 < n1 and n3 < n2:
+    print(n3)
+    if n1 < n2:
+        print(n1)
+        print(n2)
+    if n2 < n1:
+        print(n2)
+        print(n1)
+
+print("")
+print(n1)
+print(n2)
+print(n3)
---
+++ sort _simples_tentativa2..py
@@ -0,0 +1,22 @@
+n1, n2 , n3 = input(" "). split()
+n1=int(n1)
+n2=int(n2)
+n3=int(n3)
+lista_nao_ordenada=[]
+lista_ordenada=[]
+
+lista_nao_ordenada.append(n1)
+lista_nao_ordenada.append(n2)
+lista_nao_ordenada.append(n3)
+
+for valor in lista_nao_ordenada:
+    print(n1)
+    print(n2)
+    print(n3)
+
+print("   ")
+
+for valor in lista_nao_ordenada:
+    print(n1)
+    print(n2)
+    print(n3) 
